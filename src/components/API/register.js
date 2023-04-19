45e32e9e51d4aed946daf6e8ffa1ace189951273
AudunTA <aud-and2@online.no>
2023-04-19 22:41:02 +0200

    form validation sign up

--- src/components/API/register.js
+++ src/components/API/register.js
@@ -1,11 +1,35 @@
 import { baseURL } from "./baseURL";
-
-export async function handleRegister(
+const endpoint = "/auth/register";
+export async function registerUser(
   email,
   userName,
   password,
   avatar,
-  checkedManager
+  checkedManager,
+  apiErrors
 ) {
   console.log(email, userName, password, avatar, checkedManager);
+
+  const userInformation = {
+    name: userName,
+    email: email,
+    password: password,
+    avatar: avatar,
+    venueManager: checkedManager,
+  };
+  const options = {
+    method: "POST",
+    body: JSON.stringify(userInformation),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+  const request = await fetch(baseURL + endpoint, options);
+  const json = await request.json();
+  console.log(json.errors);
+  if (!request.ok) {
+    for (let i = 0; i < json.errors.length; i++) {
+      apiErrors(json.errors[i].message);
+    }
+  }
 }
--- src/components/Forms/SignUpForm/index.jsx
+++ src/components/Forms/SignUpForm/index.jsx
@@ -3,23 +3,24 @@ import { PrimaryButton } from "../../../styles/Buttons.styled";
 import { FormSignUp } from "./SignUpForm.styled";
 import ReactSwitch from "react-switch";
 import { useState } from "react";
-import { handleRegister } from "../../API/register";
+import { registerUser } from "../../API/register";
 import {
   emailInputValidation,
   userNameValidation,
   passwordValidation,
-} from "./FormValidation/inputValidation";
+} from "../FormValidation/inputValidation";
 function SingUpForm() {
   //Input states
-  const [email, setEmail] = useState();
+  const [email, setEmail] = useState("");
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [avatar, setAvatar] = useState("");
   const [checkedManager, setCheckedManager] = useState(false);
   //error states
-  const [errorEmail, setErrorEmail] = useState(false);
-  const [errorUserName, setErrorUserName] = useState(false);
-  const [errorPassword, setErrorPassword] = useState(false);
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorUserName, setErrorUserName] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorApi, setErrorApi] = useState("");
   const onEmailChange = (event) => {
     setEmail(event.target.value);
   };
@@ -37,31 +38,55 @@ function SingUpForm() {
     setCheckedManager(val);
   };
   //Sending user input to be validated before the API post request for registrer.
-  const handleRegistrer = () => {
+  const handleRegister = () => {
     //Sending userinputs to be validated
     //Response will eighter be empty if successful or contain the error message.
     const emailValidationResponse = emailInputValidation(email);
     const userNameValidationResponse = userNameValidation(userName);
     const passwordValidationResponse = passwordValidation(password);
     //if response returns error
+    let validEmail = false;
+    let validUserName = false;
+    let validPassword = false;
     if (emailValidationResponse) {
       //setting the error to the response
       setErrorEmail(emailValidationResponse);
     } else {
       //clearing the error if no errors are found, this is important as if the user attempts more inputs the error will clear if validated.
       setErrorEmail("");
+      validEmail = true;
     }
 
     if (userNameValidationResponse) {
       setErrorUserName(userNameValidationResponse);
     } else {
       setErrorUserName("");
+      validUserName = true;
     }
     if (passwordValidationResponse) {
       setErrorPassword(passwordValidationResponse);
     } else {
       setErrorPassword("");
+      validPassword = true;
     }
+    if (errorEmail) {
+      console.log("HELLO");
+    }
+    console.log("test:", errorEmail, errorUserName, errorPassword);
+    if (validEmail && validUserName && validPassword) {
+      console.log("test2", email, userName, password);
+      registerUser(
+        email,
+        userName,
+        password,
+        avatar,
+        checkedManager,
+        apiErrors
+      );
+    }
+  };
+  const apiErrors = (error) => {
+    setErrorApi(error);
   };
   return (
     <FormSignUp>
@@ -81,6 +106,7 @@ function SingUpForm() {
           placeholder="Avatar (optional)"
           onChange={onAvatarChange}
         ></input>
+        {errorApi ? <p className="error-message">{errorApi}</p> : ""}
       </div>
       <div className="manager-account">
         <p>Manager Account</p>
@@ -94,8 +120,7 @@ function SingUpForm() {
           onChange={handleManagerChange}
         />
       </div>
-
-      <PrimaryButton onClick={handleRegistrer}> Sign Up </PrimaryButton>
+      <PrimaryButton onClick={handleRegister}> Sign Up </PrimaryButton>
     </FormSignUp>
   );
 }
