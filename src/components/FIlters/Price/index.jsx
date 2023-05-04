b27be01825b4ad3e2bd8361623899e63d890ca04
AudunTA <aud-and2@online.no>
2023-05-04 23:10:34 +0200

    changed the rangeslider for price filter

--- src/components/FIlters/Price/index.jsx
+++ src/components/FIlters/Price/index.jsx
@@ -1,7 +1,93 @@
-import React from "react";
-import { Slider } from "@mui/material";
-function Price() {
-  return <Slider />;
+import * as React from "react";
+import PropTypes from "prop-types";
+import Slider, { SliderThumb } from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import { FilterContainer } from "../Filters.styled";
+
+const marks = [
+  {
+    value: 0,
+    label: "0°C",
+  },
+  {
+    value: 20,
+    label: "20°C",
+  },
+  {
+    value: 37,
+    label: "37°C",
+  },
+  {
+    value: 100,
+    label: "100°C",
+  },
+];
+
+function valuetext(value) {
+  return `${value}°C`;
 }
 
-export default Price;
+const AirbnbSlider = styled(Slider)(({ theme }) => ({
+  color: "var(--btn-color-secondary)",
+  height: 3,
+  padding: "13px 0",
+  "& .MuiSlider-thumb": {
+    height: 27,
+    width: 27,
+    backgroundColor: "#fff",
+    border: "1px solid currentColor",
+    "&:hover": {
+      boxShadow: "0 0 0 8px var(--text-color-highlight)",
+    },
+    "& .airbnb-bar": {
+      height: 9,
+      width: 1,
+      backgroundColor: "currentColor",
+      marginLeft: 1,
+      marginRight: 1,
+    },
+  },
+  "& .MuiSlider-track": {
+    height: 3,
+  },
+  "& .MuiSlider-rail": {
+    color: "#bfbfbf",
+    opacity: "dark",
+    height: 3,
+  },
+}));
+
+function AirbnbThumbComponent(props) {
+  const { children, ...other } = props;
+  return (
+    <SliderThumb {...other}>
+      {children}
+      <span className="airbnb-bar" />
+      <span className="airbnb-bar" />
+      <span className="airbnb-bar" />
+    </SliderThumb>
+  );
+}
+
+AirbnbThumbComponent.propTypes = {
+  children: PropTypes.node,
+};
+
+export default function Price() {
+  return (
+    <FilterContainer>
+      <Typography gutterBottom>Airbnb</Typography>
+      <AirbnbSlider
+        aria-label="Always visible"
+        getAriaValueText={valuetext}
+        slots={{ thumb: AirbnbThumbComponent }}
+        valueLabelDisplay="on"
+        getAriaLabel={(index) =>
+          index === 0 ? "Minimum price" : "Maximum price"
+        }
+        defaultValue={[20, 40]}
+      />
+    </FilterContainer>
+  );
+}
