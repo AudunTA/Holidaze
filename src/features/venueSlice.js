24841c569029e8fb7284dc8c2119553dd67c6b9a
AudunTA <aud-and2@online.no>
2023-05-30 22:40:24 +0200

    cleaned up readme

--- src/features/venueSlice.js
+++ src/features/venueSlice.js
@@ -82,7 +82,7 @@ export const venueSlice = createSlice({
         //checks if filters are active
         state.filterMessage = `note: filters are active`;
         if (state.filter.search) {
-          state.filterMessage = `result for search ${state.filter.search} and filters`;
+          state.filterMessage = `result for search: "${state.filter.search}" + active filters`;
         }
       }
     },
